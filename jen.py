5c21aba46b0c778e7b53baa282ea97f957975759
vipvenkat <95736679+vipvenkat@users.noreply.github.com>
2023-07-14 16:22:29 +0530

    Create jen.py

---
+++ jen.py
@@ -0,0 +1,41 @@
+# Addition
+num1 = 10
+num2 = 5
+result = num1 + num2
+print("Addition Result:", result)
+
+# Subtraction
+num1 = 10
+num2 = 5
+result = num1 - num2
+print("Subtraction Result:", result)
+
+# Multiplication
+num1 = 10
+num2 = 5
+result = num1 * num2
+print("Multiplication Result:", result)
+
+# Division
+num1 = 10
+num2 = 5
+result = num1 / num2
+print("Division Result:", result)
+
+# Integer Division
+num1 = 10
+num2 = 5
+result = num1 // num2
+print("Integer Division Result:", result)
+
+# Modulo (Remainder)
+num1 = 10
+num2 = 5
+result = num1 % num2
+print("Modulo Result:", result)
+
+# Exponentiation
+num1 = 2
+num2 = 3
+result = num1 ** num2
+print("Exponentiation Result:", result)
